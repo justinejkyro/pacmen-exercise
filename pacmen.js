36a64a77339a2c31b16dc7fc92ad3cfaee87dbe7
Justine Kyro <118939886+justinejkyro@users.noreply.github.com>
2023-03-17 09:00:41 -0500

    Update pacmen.js

--- pacmen.js
+++ pacmen.js
@@ -16,8 +16,7 @@ function setToRandom(scale) {
 
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
-  // returns an object with random values scaled {x: 33, y: 21}
-  let velocity = setToRandom(10); // {x:?, y:?}
+  let velocity = setToRandom(10); 
   let position = setToRandom(200);
 
 
